e8a3744494e5ead5b3ed90797e1f90e98533b54d
hamu <nishimura@west.sd.keio.ac.jp>
2016-03-29 17:32:27 +0900

    Update table_reader1.v

--- table_reader1.v
+++ table_reader1.v
@@ -1,14 +1,16 @@
 //TABLE_READER1.v
-module TABLE_READER1(CLK, RST, EN, NOW_STATE_IN, NOW_STATE_OUT);
+module TABLE_READER1(CLK, RST, EN, NOW_STATE_IN, NOW_STATE_OUT, EN_MATCH);
 input CLK;
 input RST;
 input EN;
 input [7:0] NOW_STATE_IN;
 output [7:0] NOW_STATE_OUT;
-integer j;
+output EN_MATCH;
+integer i,j;
 
-reg [7:0] ADDR;
+reg [7:0] ADDR [0:31];
 reg [7:0] NOW_STATE_OUT;
+reg EN_MATCH;
 reg [7:0] RAM_CURRENT_STATE_G[0:31];
 reg [3:0] RAM_CHARA[0:31];
 reg [7:0] RAM_NEXT_STATE[0:31];
@@ -23,23 +25,26 @@ function PROCESS_STRING1;
   input EN;
   input CHARA_EN;
   begin
-if(EN == 1) begin // TABLE_READER.vから信号受け取ってる
-  for(j=0; j<4; j=j+1) begin
-    if(RAM_CURRENT_STATE_G[j] == NOW_STATE_IN) begin
-      ADDR = j;
-      CHARA_EN = 1; //flug on
+    if(EN == 1) begin // TABLE_READER.vから信号受け取ってる
+      j = 0;
+      for(i=0; i<11; i=i+1) begin
+        if(RAM_CURRENT_STATE_G[i] == NOW_STATE_IN) begin
+          ADDR[j] = i;
+          j = j + 1;
+          CHARA_EN = 1; //flug on
+        end
+      end
     end
-  end
-end
-if(CHARA_EN == 1) begin
-  if(RAM_CHARA[ADDR] == 12) begin // テキストデータを入れる予定
-    NOW_STATE_OUT = RAM_NEXT_STATE[ADDR];
-  end else if (RAM_FAILURE_STATE[NOW_STATE_IN - 1] == 0) begin
-      NOW_STATE_OUT = 0;
+    if(CHARA_EN == 1) begin
+      if(RAM_CHARA[ADDR[0]] == 12) begin // テキストデータを入れる予定
+        NOW_STATE_OUT = RAM_NEXT_STATE[ADDR[0]];
+        EN_MATCH = 1;
+      end else if (RAM_FAILURE_STATE[NOW_STATE_IN - 1] == 0) begin
+        NOW_STATE_OUT = 0;
       end else 
         NOW_STATE_OUT = RAM_FAILURE_STATE[NOW_STATE_IN - 1];
+    end
   end
-end
 endfunction
 
 assign SEARCH_OUT1 = PROCESS_STRING1(EN, CHARA_EN);
