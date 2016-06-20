ac7d047007d6b7738628dd61b6e865132123cf5a
hamu <nishimura@west.sd.keio.ac.jp>
2016-06-20 22:24:27 +0900

    Update register.v

--- register.v
+++ register.v
@@ -1,20 +1,22 @@
 //REGISTER.v
-//状態遷移の情報を格納するモジュール
-module REGISTER(CLK, RST, EN, EN1,  NOW_STATE, NOW_STATE1);
+module REGISTER(CLK, RST, EN, NOW_STATE, STATE, MATCHING_START);
 input CLK;
 input RST;
-input EN, EN1;
+input EN;
 input [7:0] NOW_STATE;
-input [7:0] NOW_STATE1;
-
+output [7:0] STATE;
 reg [7:0] STATE;
+output MATCHING_START;
+reg MATCHING_START;
+
 
 always@ (posedge CLK) begin
   if(EN == 1)begin
     STATE <= NOW_STATE;
-  end
-  if(EN1 == 1) begin
-    STATE <= NOW_STATE1;
-  end
+    MATCHING_START = 1;
+  end else
+    STATE = 0;
+    MATCHING_START = 1;
 end
+
 endmodule
